78734ccf0905b19243210a0795d3065f607b0802
Vincent Ahn <jmahn97@hotmail.com>
2021-06-15 17:51:08 -0400

    More Tic Tac Toe

--- skeleton/src/index.js
+++ skeleton/src/index.js
@@ -1,6 +1,9 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view.js'); // require appropriate file
+const Game = require('./../ttt_node/game.js'); // require appropriate file
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  console.log("DOM Loaded.");
+  let el = document.querySelector(".ttt");
+  let view = new View(new Game(), el);
 });
--- skeleton/src/ttt-view.js
+++ skeleton/src/ttt-view.js
@@ -1,11 +1,40 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    // Create unordered list
+    // debugger;
+    let unorderedList = document.createElement("ul");
+
+    // Create 9 li elements (store position as data-pos)
+    // Set border on li elements
+    for(let x = 0; x < 3; x++){
+      for(let y = 0; y < 3; y++){
+        // debugger;
+        let listElement = document.createElement("li");
+        listElement.setAttribute("data-pos", `[${x}, ${y}]`);
+
+        // listElement.addEventListener('click', this.handleClick);
+
+        unorderedList.append(listElement);
+      }
+    }
+
+    this.el.append(unorderedList);
+  }
   
-  bindEvents() {}
+  bindEvents() {
+    
+  }
 
-  handleClick(e) {}
+  handleClick(e) {
+    const item = e.target;
+    item.classList.toggle("unclicked");
+  }
 
   makeMove(square) {}
 
